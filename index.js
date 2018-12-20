497cd1a667c3e85b7881933649aa6d68083ceb52
brazzmo <43312020+brazzmo@users.noreply.github.com>
2018-12-19 21:20:41 -0700

    Fixed some code errors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 
-module.exports.hook = (channel, name, {content, username, avatar, color, title, description, footer, author, image, thumbnail, fields}) => {
+module.exports.hook = async (channel, name, {content, username, avatar, color, title, description, footer, author, image, thumbnail, fields}) => {
+  if(!channel) throw new TypeError(`[easyhook] Include a valid channel object!`)
+  if(!typeof name === 'string') throw new TypeError(`[easyhook] Name must be a string!`)
     let contents = {
             username: username,
             avatarURL: avatar,
@@ -18,9 +20,9 @@ module.exports.hook = (channel, name, {content, username, avatar, color, title, 
                     ]
           }
      
-    channel.fetchWebhooks().then(webhook => {
+    let hook = await channel.fetchWebhooks().then(webhook => {
       let hook = webhook.find(h => h.name === name)
-      if(!hook) channel.createWebhook(name, {
+      if(!hook) hook = await channel.createWebhook(name, {
         avatar: avatar
       }).then(webhook => {
           webhook.send(content, contents).catch(err => {
@@ -33,29 +35,44 @@ module.exports.hook = (channel, name, {content, username, avatar, color, title, 
           })
       }
     })
+    return hook;
   }
 
   module.exports.create = async (channel, name, avatarURL) => {
-      let webhooks = await channel.fetchWebhooks()
-      if(webhooks.length >= 10) throw new TypeError('Channels can\'t have more than 10 webhooks, delete some and try again.')
-      channel.createWebhook(name, {
-          avatar: avatarURL
-      }).catch(err => {
-          throw new TypeError(`Webhook creation unsuccessful, reason: ${err.message}`)
-      }).then(console.log(`[easyhook] Webhook called ${name} successfully created in channel: ${channel.name}`))
-  }
+    const channelTypes = [
+      'dm',
+      'group',
+      'text',
+      'voice',
+      'category'
+    ]
+    if(!channel.type) throw new TypeError(`[easyhook] You must include a valid channel!`);
+    if(!channelTypes.includes(channel.type)) throw new TypeError(`[easyhook] You must include a valid channel!`);
+    if(!typeof(name) === 'string') throw new TypeError(`[easyhook] You must include a name for the webhook!`);
+    if(!avatarURL) throw new TypeError(`[easyhook] You must include a URL for the webhook's avatar!`)
+    let webhooks = await channel.fetchWebhooks()
+    if(webhooks.length >= 10) throw new TypeError('Channels can\'t have more than 10 webhooks, delete some and try again.')
+    const hook = await channel.createWebhook(name, {
+        avatar: avatarURL
+    }).catch(err => {
+        throw new TypeError(`Webhook creation unsuccessful, reason: ${err.message}`)
+    })
+    
+    return hook;
+}
 
   module.exports.edit = async (channel, webhook, {newName, newAvatar, newChannel}) => {
     channel.fetchWebhooks().then(webhooks => {
-      let hook = webhooks.find(u => u.name === webhook)
-      if(hooks.length <= 0) throw new TypeError(`[easyhook] Hook called ${name} was not found, check punctuation and spelling, or create one.`)
-      hook.edit({
+      let hook = webhooks.find(u => u.name === webhook) || webhooks.get(webhook);
+      if(!hook) throw new TypeError(`[easyhook] Hook was not found.`)
+      let edit = await hook.edit({
         name: newName,
-        avatar: newAvatar,
-        channel: newChannel,
+        avatar: newAvatar
       }).catch(err => {
         throw new TypeError(`[easyhook] Failed to edit webhook, reason: ${err.message}`)
-      }).then(console.log(`[easyhook] Webhook (${webhook}) successfully edited!`))
+      })
+      return edit;
     })
+    
   }
 
